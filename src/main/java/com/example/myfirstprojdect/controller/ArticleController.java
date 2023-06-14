5c183fbfe0827b832ea8e493d5bc8fe011c77dc4
juhyejin <juhyejin.dev@gmail.com>
2023-06-14 15:25:03 +0900

    데이터 베이스에서 정보를 받아서 뿌려주기

--- src/main/java/com/example/myfirstprojdect/controller/ArticleController.java
+++ src/main/java/com/example/myfirstprojdect/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.myfirstprojdect.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -31,5 +35,17 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = "+ id);
+        //id를 데이터로 가져옴!
+        //Optional<Article> articleEntity = articleRepository.findById(id); 이렇게도 쓸수 있다
+         Article articleEntity = articleRepository.findById(id).orElse(null);
+        //가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+        // 보여줄 페이지를 설정
+        return "articles/show";
     }
 }
