c83a0545de0382475b2e797f6b6698b7e075622a
juhyejin <juhyejin.dev@gmail.com>
2023-06-14 15:01:56 +0900

    lombok이용하여 리팩토링

--- src/main/java/com/example/myfirstprojdect/controller/ArticleController.java
+++ src/main/java/com/example/myfirstprojdect/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.myfirstprojdect.controller;
 import com.example.myfirstprojdect.dto.ArticleFormDTO;
 import com.example.myfirstprojdect.entity.Article;
 import com.example.myfirstprojdect.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
 
     @Autowired // 스프링부트가 미리 생성해 놓은 객체를 가져다가 자동 연결!
@@ -21,15 +23,13 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleFormDTO formDTO){
-        System.out.println(formDTO.toString());
-
         // 1. DTO 변환! Entity!
+        log.info(formDTO.toString());
         Article article = formDTO.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
         // 2. Repository에게 Entity를 DB안에 저장하게 함!
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-
+        log.info(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/myfirstprojdect/dto/ArticleFormDTO.java
+++ src/main/java/com/example/myfirstprojdect/dto/ArticleFormDTO.java
@@ -1,21 +1,14 @@
 package com.example.myfirstprojdect.dto;
 
 import com.example.myfirstprojdect.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleFormDTO {
     private String title;
     private String content;
-    public ArticleFormDTO(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-    @Override
-    public String toString() {
-        return "ArticleFormDTO{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null,title,content);
